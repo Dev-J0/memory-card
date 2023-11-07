92b34cc35c5f0e454f52ee08a5e04688b2bac50f
Dev-J0 <dityx.obsidian@gmail.com>
2023-11-07 16:58:32 +0100

    fetched api from giphy

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import './assets/style/style.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [giphyData, setGiphyData] = useState([]);
 
+   useEffect(() => {
+    const apiKey= 'QJCYmk1aUKV5nz5645nc6DGC64iaW62b';
+    const searchKeyword = 'iron man';
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchKeyword}&limit=15`;
+
+    axios
+      .get(endpoint)
+      .then((response) => {
+        setGiphyData(response.data.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data', error);
+       });
+   }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1> Iron Man FTW</h1>
+    <ul>
+      {giphyData.map((gifi => (
+        <li key={gifi.id}>
+          <img src={gifi.images.fixed_height.url} alt={gifi.title} />
+        </li>
+      )
+        ))}
+    </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
